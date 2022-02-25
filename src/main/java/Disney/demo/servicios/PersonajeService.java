7cb9e4c8ada0f71d457310293d6fa665e637d859
NoviceYh <aguirreee21@gmail.com>
2022-02-25 13:11:25 -0300

    creando personaje servicio base

--- src/main/java/Disney/demo/servicios/PersonajeService.java
+++ src/main/java/Disney/demo/servicios/PersonajeService.java
@@ -1,11 +1,27 @@
 
 package Disney.demo.servicios;
 
+import Disney.demo.entidades.Personaje;
+import Disney.demo.repositorios.PersonajeRepository;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonajeService {
     
+    @Autowired
+    private PersonajeRepository personajeRepository;
     
+    @Transactional
+    public Personaje save(Personaje personaje){
+        
+        
+        return personajeRepository.save(personaje);
+    }
+    
+    public void validar(Personaje personaje){
+        
+    }
     
 }
