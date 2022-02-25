13f9a6901fdf782267ef9be6feadceb49a257478
NoviceYh <aguirreee21@gmail.com>
2022-02-25 12:09:38 -0300

    constructores, getters and setters

--- src/main/java/Disney/demo/entidades/Pelicula.java
+++ src/main/java/Disney/demo/entidades/Pelicula.java
@@ -1,6 +1,8 @@
 
 package Disney.demo.entidades;
 
+import java.util.Date;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -20,4 +22,66 @@ public class Pelicula {
     private Integer calificacion;
     
     private List<Personaje> personajes;
+
+    public Pelicula() {
+    }
+
+    public Pelicula(String id, byte[] imagen, String titulo, Date fechaCreacion, Integer calificacion, List<Personaje> personajes) {
+        this.id = id;
+        this.imagen = imagen;
+        this.titulo = titulo;
+        this.fechaCreacion = fechaCreacion;
+        this.calificacion = calificacion;
+        this.personajes = personajes;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public byte[] getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(byte[] imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Integer getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(Integer calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public List<Personaje> getPersonajes() {
+        return personajes;
+    }
+
+    public void setPersonajes(List<Personaje> personajes) {
+        this.personajes = personajes;
+    }
+    
+    
 }
--- src/main/java/Disney/demo/entidades/Personaje.java
+++ src/main/java/Disney/demo/entidades/Personaje.java
@@ -1,14 +1,96 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Disney.demo.entidades;
 
-/**
- *
- * @author Alejandro
- */
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
 public class Personaje {
+    
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    
+    private byte[] imagen;
+    private String nombre;
+    private String edad;
+    private String peso;
+    private String historia;
+    
+    private Pelicula pelicula;
+
+    public Personaje() {
+    }
+
+    public Personaje(String id, byte[] imagen, String nombre, String edad, String peso, String historia, Pelicula pelicula) {
+        this.id = id;
+        this.imagen = imagen;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+        this.historia = historia;
+        this.pelicula = pelicula;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public byte[] getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(byte[] imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
+
+    public String getHistoria() {
+        return historia;
+    }
+
+    public void setHistoria(String historia) {
+        this.historia = historia;
+    }
+
+    public Pelicula getPelicula() {
+        return pelicula;
+    }
+
+    public void setPelicula(Pelicula pelicula) {
+        this.pelicula = pelicula;
+    }
+    
+    
     
 }
